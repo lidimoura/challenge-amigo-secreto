78f783c077b8badcfc6f9bb32f2ddb348402ef32
lidimoura <lidimfc@gmail.com>
2025-09-23 16:48:42 -0400

    Corrigir if(!lista) e adicionar função sortearAmigo

--- app.js
+++ app.js
@@ -17,5 +17,53 @@ function adicionarAmigo() {
     input.value = ""; //limpa o campo de input
     
     atualizarLista();
-
+    
 }
+
+//Função para atualizar a lista de amigos na tela
+function atualizarLista() {
+    //Seleciona o elemento (ul) onde os itens serão exibidos
+    const lista = document.getElementById("listaAmigos");
+
+    //Se o elemento não existir, interrompe para evitar erros
+     if (!lista) {
+        console.warn("Elemento '#listaAmigos' não encontrado.");
+        return;
+    }
+
+    //Limpa a lista antes de recomeçar o sorteio 
+    lista.innerHTML = "";
+
+    //Percorre o array de amigos e cria um <li> para cada amigo
+    for (let i = 0; i < amigos.length; i++) {
+    const li = document.createElement("li"); //cria o item
+    li.textContent = amigos[i]; //contéudo do item
+    lista.appendChild(li); //adiciona o item na lista
+    }
+}
+
+//Função para sortear um amigo da lista
+function sortearAmigo() {
+    //Seleciona o elemento onde o resultado será exibido
+    const resultado = document.getElementById("resultado");
+
+    //Verifica se o array de amigos não está vazio
+    if (amigos.length === 0) {
+        alert("Adicione pelo menos um amigo antes de sortear.");
+        return;
+    }   
+
+    //Gera um indice aleatório dentro do array amigos
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+
+    //Obtém o nome do amigo sorteado
+    const amigoSorteado = amigos[indiceAleatorio];
+
+    //Exiibe o resultado na tela
+    if (resultado) {
+        resultado.innerHTML = `<lis>${amigoSorteado}<li>`;
+    } else {
+        console.warn("Elemento ´#resultado´ não encontrado.");
+    }
+}
+
